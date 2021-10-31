ab2e6ef0fd29a8c0a8ed5cd8483c51a521cd3544
ahp-sooyaa <sooyaa894@gmail.com>
2021-10-31 10:24:07 +0630

    Add public compiled assets back
    Fix the mix content error in heroku by scheme to https
    Update db connection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    // testing haha
+
     /**
      * Register any application services.
      *
@@ -23,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        URL::forceScheme('https');
     }
 }
