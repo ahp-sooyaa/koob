d01173497b1bda75e8e1443369b3e9f98973f963
ahp-sooyaa <sooyaa894@gmail.com>
2022-12-31 17:49:47 +0630

    Add og tag for linkedin

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="title" property="og:title" content="Koob demo site">
+        <meta name="type" property="og:type" content="website">
+        <meta name="image" property="og:image" content="https://koobdemo.site/images/hero_image_large.jpeg">
+        <meta name="url" property="og:url" content="https://koobdemo.site">
+        <meta name="description" property="og:description" content="Koob is a ecommerce built with laravel, vue.">
+
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png">
@@ -18,9 +24,6 @@
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
         <link rel="stylesheet" onload="this.onload=null;this.removeAttribute('media');"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
-        {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> --}}
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
